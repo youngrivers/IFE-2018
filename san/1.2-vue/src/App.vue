7ebc4b29683cf9664ba3ddaa1e88f83df721df55
youngrivers <401712228@qq.com>
2018-11-22 01:14:34 +0800

    1.2-vue

---
+++ san/1.2-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="hello">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'this is a test'
+            }
+        }
+    }
+</script>
+<style>
+#hello{
+    color: blueviolet;
+}
+</style>
