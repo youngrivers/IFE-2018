c47a9a32182882d7a9da539c33a4341b4f3cfe88
youngrivers <401712228@qq.com>
2018-11-28 23:30:16 +0800

    3.1-vue

---
+++ san/3.1-vue/src/app.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <button v-on:click='show=!show'>
+      Toggle
+    </button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+    <transition name="slide-fade">
+      <p v-if="show">NMLS</p>
+    </transition>
+    <transition name=bounce>
+      <p v-if="show">bdjbvdksbvdshbvjsbvksvkdbhsvbdksbvkdsbvksdjk</p>
+    </transition>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        show:true,
+          }
+      }
+  }
+</script>
+<style>
+.fade-enter-active, .fade-leave-active{
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to{
+  opacity: 0;
+}
+.slide-fade-enter-active{
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active{
+  transition: all .8s cubic-bezier(1, 0, 0, 1)
+}
+.slide-fade-enter, .slide-fade-leave-to{
+  transform: translateX(10px);
+  opacity: 0;
+}
+.bounce-enter-active{
+  animation: bounce-in .5s;
+}
+.bounce-leave-active{
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0%{
+    transform: scale(0);
+  }50%{
+    transform: scale(1.5);
+  }100%{
+    transform: scale(1);
+  }
+}
+</style>
