5c8e927ee663ccbdaf0a18b5a995eceeacd8c4b3
youngrivers <401712228@qq.com>
2018-12-24 13:31:43 +0800

    3.1-vue-c

---
+++ san/3.1-vue-c/README.md
@@ -0,0 +1,14 @@
+# 3.1-vue
+> 用递归的方式实现级联组件
+## 项目构建
+ ```
+ npm init-y //快速构建json
+ yarn install //安装依赖
+ ```
+## 运行
+```
+# 本地运行 at localhost:8080
+npm run dev
+# 打包
+npm run build
+```
---
+++ san/3.1-vue-c/src/app.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="performanceBox">
+          <!-- 级联选择器多选 -->
+          <choiceindex v-on:CheckedsIndexCodes="FromTreeCheckeds"></choiceindex>
+  </div>
+</template>
+<script>
+//引用上边创建的MultipleChoice文件夹下的index出口文件就好了。
+import choiceindex from "./components/index.vue"; //级联选择多选 完成
+export default {
+  components: {
+    choiceindex,
+  },
+  data() {
+    return {
+      SaveCascadeIndexCodes: [], //保存级联选择器多选的基准code
+      SaveJiZhunParams: [], //保存业绩表现需要的参数
+
+    };
+  },
+  methods: {
+    //多选选择基准时的code
+    FromTreeCheckeds(IndexCodes) {
+//IndexCodes就是选中的item的数组，操作他就好了
+      // console.log(IndexCodes);
+      this.SaveCascadeIndexCodes = IndexCodes;
+    },
+  }
+};
+</script>
+<style  rel="stylesheet/scss" lang="scss">
+</style>
