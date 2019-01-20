df7c7898678169dfd40da065716e2968292b9f86
youngrivers <401712228@qq.com>
2019-01-20 23:56:36 +0800

    4.4-vue

---
+++ san/4.4-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ui-todo></ui-todo>
+  </div>
+</template>
+<script>
+import Todo from './components/Todo.vue'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components:{
+    'uiTodo':Todo
+  }
+}
+</script>
---
+++ san/4.4-vue/src/routers/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//compents
+import todo from '../components/Todo.vue'
+Vue.use(VueRouter)
+const routes = [
+    { path: "/all", component: todo },
+    {
+        path: "/active",
+        component: todo
+    },
+    { path: '/', redirect: '/all' }
+]
+const router = new VueRouter({
+    routes
+});
+export default router;
